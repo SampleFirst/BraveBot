dbec040a0528cc29ae80e7b8a989171656b1d7a6
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-05-10 17:29:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -54,20 +54,35 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 # Others
 IS_VERIFY = is_enabled((environ.get('IS_VERIFY', 'False')), False)
 HOW_TO_VERIFY = environ.get('HOW_TO_VERIFY', "https://t.me/+IvrcMfPKCMxkNjVl")
-SHORTLINK_URL = environ.get('SHORTLINK_URL', "kingurl.in")
-SHORTLINK_API = environ.get('SHORTLINK_API', "d5be075758edd71808de5e98b339af720e5eabce")
 
-VERIFY1_URL = environ.get('VERIFY1_URL', "omnifly.in.net")
+# Verification 1
+SHORT1_URL = environ.get('SHORT1_URL', "ziplinker.net") # oneman
+SHORT1_API = environ.get('SHORT1_API', "603c7a037b8bf4d5a9d2eaa43df6b84783da7aa6")
+
+SHORT2_URL = environ.get('SHORT2_URL', "omnifly.in.net") # Second
+SHORT2_API = environ.get('SHORT2_API', "579a68527328d450fea22397f9b8890c2e198cab")
+
+SHORT3_URL = environ.get('SHORT3_URL', "ziplinker.net") # Fourth 
+SHORT3_API = environ.get('SHORT3_API', "0d2e001687e2a100f98cb027866a13d5214bd6c0")
+
+SHORT4_URL = environ.get('SHORT4_URL', "omnifly.in.net") # Fifth
+SHORT4_API = environ.get('SHORT4_API', "4294eebd727e09d894b8870419f9dd4112fb6fec")
+
+# Verification 2
+VERIFY1_URL = environ.get('VERIFY1_URL', "omnifly.in.net") # Frist
 VERIFY1_API = environ.get('VERIFY1_API', "2f07494c29d95eacffbc5d6998bc56c91b8ee47d")
 
-VERIFY2_URL = environ.get('VERIFY2_URL', "ziplinker.net")
+VERIFY2_URL = environ.get('VERIFY2_URL', "ziplinker.net") # Adventure 
 VERIFY2_API = environ.get('VERIFY2_API', "12f1784cd5831936c8e4c6f5e414a2f31a22eb2f")
 
-VERIFY3_URL = environ.get('VERIFY3_URL', "omnifly.in.net")
-VERIFY3_API = environ.get('VERIFY3_API', "4294eebd727e09d894b8870419f9dd4112fb6fec")
+VERIFY3_URL = environ.get('VERIFY3_URL', "omnifly.in.net") # Fourth 
+VERIFY3_API = environ.get('VERIFY3_API', "df8b5a6b74fda240a993dc4c4fdd07d8763914b9")
 
-VERIFY4_URL = environ.get('VERIFY4_URL', "instantearn.in")
-VERIFY4_API = environ.get('VERIFY4_API', "667b11aeda738ac4c494084055faff1f636f8534")
+VERIFY4_URL = environ.get('VERIFY4_URL', "ziplinker.net") # Fifth
+VERIFY4_API = environ.get('VERIFY4_API', "f96c0aa1dadc48971de09d64d3909d3cc331326a")
+
+LOG_CHAT = int(environ.get('LOG_CHAT', 0))
+LOG_GRP = int(environ.get('LOG_GRP', 0))
 
 IS_SHORTLINK = is_enabled((environ.get('IS_SHORTLINK', 'False')), False)
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
