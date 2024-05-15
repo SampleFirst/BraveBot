ca9e61100273e7763f32854c51a7a401fa532c2b
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-05-15 22:19:39 +0530

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -15,7 +15,7 @@ logger.setLevel(logging.ERROR)
 from utils import temp, get_verify_status, update_similer_status
 
 @Client.on_message(filters.command('update'))
-async def update_user_very(client, message):
+async def update_user_very(bot, message):
     text = message.text.split()
     tz = pytz.timezone('Asia/Kolkata')
     date_pack = datetime.now(tz)
@@ -28,8 +28,8 @@ async def update_user_very(client, message):
             status = await get_verify_status(user_id)
             date_var = status["date"]
             time_var = status["time"]
-            await update_similer_status(client, user_id, short_var, date_var, time_var)
-            await client.send_message(
+            await update_similer_status(bot, user_id, short_var, date_var, time_var)
+            await bot.send_message(
                 chat_id=LOG_CHANNEL,
                 text=f"#UpdateForUser\n"
                      f"User id: {user_id}\n"
@@ -40,7 +40,7 @@ async def update_user_very(client, message):
                      f"Me: {temp.U_NAME}."
             )
         else:
-            await client.send_message(
+            await bot.send_message(
                 chat_id=LOG_CHANNEL,
                 text=f"#UserNotFound\n"
                      f"User id: {user_id}\n"
