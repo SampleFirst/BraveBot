c5ffd5be1d573f691418aa85347165f6634951fe
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-05-15 18:11:03 +0530

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -14,33 +14,33 @@ from utils import temp, get_verify_status, update_verify_status
 
 @Client.on_message(filters.command('update'))
 async def update_user_very(client, message):
-    name = temp.U_NAME
     text = message.text.split("_")
     if len(text) == 2:
         user_id = text[0]
         short_var = text[1]
-        if await db.is_user_exist(user_id):
-            status = await get_verify_status(user_id)
-            date_var = status["date"]
-            time_var = status["time"]
-            
-            await update_verify_status(client, user_id, short_var, date_var, time_var)
-            
-            # Reply to the message
-            await client.send_message(
-                chat_id=message.chat.id,
-                text=f"#UpdateForUser\n"
-                     f"User id: {user_id}\n"
-                     f"short number: {short_var}\n"
-                     f"Me: {name}."
-            )
-        else:
-            await client.send_message(
-                chat_id=message.chat.id,
-                text=f"#UserNotFound\n"
-                     f"user id: {user_id}\n"
-                     f"Me: {name}"
-            )
+        chat_type = message.chat.type
+        if chat_type == enums.ChatType.PRIVATE:
+            await message.reply_text("Only For Groups", quote=True)
+        elif chat_type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
+            if await db.is_user_exist(user_id):
+                status = await get_verify_status(user_id)
+                date_var = status["date"]
+                time_var = status["time"]
+                await update_verify_status(client, user_id, short_var, date_var, time_var)
+                await client.send_message(
+                    chat_id=message.chat.id,
+                    text=f"#UpdateForUser\n"
+                         f"User id: {user_id}\n"
+                         f"short number: {short_var}\n"
+                         f"Me: {temp.U_NAME}."
+                )
+            else:
+                await client.send_message(
+                    chat_id=message.chat.id,
+                    text=f"#UserNotFound\n"
+                         f"user id: {user_id}\n"
+                         f"Me: {temp.U_NAME}"
+                )
 
 @Client.on_message(filters.command('id'))
 async def showid(client, message):
