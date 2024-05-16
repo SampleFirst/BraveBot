f879f6cf169d39775e6cfaf903260aa6bf897ed9
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-05-17 03:12:45 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -817,7 +817,7 @@ async def verify_user(bot, userid, token):
         date_var = datetime.now(tz)
         temp_time = date_var.strftime("%H:%M:%S")
         date_var, time_var = str(date_var).split(" ")
-    await update_special_verify_status(bot, user.id, vrnum, date_var, temp_time)
+    await update_verify_status(bot, user.id, vrnum, date_var, temp_time)
 
 
 async def check_verification(bot, userid):
